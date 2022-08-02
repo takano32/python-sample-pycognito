f9ea7fa1606506f428dc9515d5bc256f5332bc78
TAKANO Mitsuhiro <takano32@gmail.com>
2022-08-02 16:37:27 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,24 @@
+import json
+import os
+
+import requests
+from dotenv import load_dotenv
+from pycognito.utils import RequestsSrpAuth
+
+dotenv_path = os.path.join(os.path.dirname(__file__), ".env")
+load_dotenv(dotenv_path)
+
+auth = RequestsSrpAuth(
+    username=os.getenv("USERNAME"),
+    password=os.getenv("PASSWORD"),
+    user_pool_id=os.getenv("USER_POOL_ID"),
+    client_id=os.getenv("APP_CLIENT_ID"),
+    user_pool_region=os.getenv("USER_POOL_REGION"),
+)
+
+end_point = os.getenv("ORION_ENDPOINT")
+response = requests.get(end_point + "/version", auth=auth)
+
+
+str_json = json.dumps(response.json(), indent=4)
+print(str_json)
